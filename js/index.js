5b41fc85cc72ba1909a5fe39858d6be9d8e42776
Frank Martinez <frankie.martinez@gmail.com>
2019-02-05 16:23:27 -0800

    style

---
+++ js/index.js
@@ -0,0 +1,23 @@
+
+// Build a component because that's what we do
+class Something {
+    constructor(element) {
+        this.element = element;
+    }
+}
+
+
+
+
+// Select some stuff
+const iconsLeft = document.querySelectorAll('.icon-left');
+iconsLeft.forEach(icon => console.log(icon));
+
+
+
+
+
+
+
+
+// iconsLeft.forEach( link => new TabLink(link) );
