7b5dd52371363c931dd03b9ed48a631df1db3d15
Frank Martinez <frankie.martinez@gmail.com>
2019-02-06 11:03:55 -0800

    minor adjustments to animate

--- js/index.js
+++ js/index.js
@@ -1,63 +1,74 @@
-let last_known_scroll_position = 0;
+// Starting points for variables
+let last_known_scroll_position = 0; 
 let ticking = false;
 
-// Build a component because that's what we do
-class IconSlide {
-  constructor(icon) {
-    this.icon = icon;
+
+// Slide from left component
+class IconSlideLeft {
+  constructor(icon, scroller) {
+    this.icon = icon; // This doesn't do anything
     this.scrollPos = last_known_scroll_position;
 
     this.iconLeft = document.querySelectorAll('.icon-left');
     this.textLeft = document.querySelectorAll('.text-left');
     
-    this.iconRight = document.querySelectorAll('.icon-right');
-    this.textRight = document.querySelectorAll('.text-right');
-    
     this.iconLeft.forEach(icon => { 
-      if (this.scrollPos > 450) {
+      if (this.scrollPos > scroller) {
         icon.style = 'left: 0';
       }
-      if (this.scrollPos < 400) {
+      if (this.scrollPos < (scroller - 50)) {
         icon.style = 'left: -1000px';
       }
     });
 
     this.textLeft.forEach(text => { 
-      if (this.scrollPos > 400) {
+      if (this.scrollPos > scroller) {
         text.style = 'left: 0';
       }
-      if (this.scrollPos < 355) {
+      if (this.scrollPos < (scroller - 60)) {
         text.style = 'left: -1500px';
       }
     });
+  }
+}
+
+// Slide from right component
+class IconSlideRight {
+  constructor(icon, scroller) {
+    this.icon = icon;
+    this.scrollPos = last_known_scroll_position;
+
+    this.iconRight = document.querySelectorAll('.icon-right');
+    this.textRight = document.querySelectorAll('.text-right');
 
     this.iconRight.forEach(icon => { 
-      if (this.scrollPos > 550) {
+      if (this.scrollPos > scroller) {
         icon.style = 'right: 0';
       }
-      if (this.scrollPos < 525) {
+      if (this.scrollPos < (scroller - 50)) {
         icon.style = 'right: -1000px';
       }
     });
 
     this.textRight.forEach(text => { 
-      if (this.scrollPos > 500) {
+      if (this.scrollPos > scroller) {
         text.style = 'right: 0';
       }
-      if (this.scrollPos < 475) {
+      if (this.scrollPos < (scroller - 60)) {
         text.style = 'right: -1500px';
       }
     });
-
-
   }
 }
 
-
 // Scroll Position
 // Shove in the last known position
-function doSomething() {
-  scroll_pos = new IconSlide(last_known_scroll_position);
+// The number correlates to scroller in the constructor
+// Scroller needs to be the starting point for the elements animation
+// Scroller is position from the top.
+doSomething = () => {
+  scroll_pos = new IconSlideLeft(last_known_scroll_position, 400);
+  scroll_pos = new IconSlideRight(last_known_scroll_position, 500);
 }
 
 window.addEventListener('scroll', () => {
@@ -72,11 +83,5 @@ window.addEventListener('scroll', () => {
     ticking = true;
   }
 });
-
-
-// Select some stuff
-const iconsSlider = document.querySelectorAll('.icon-slider');
-iconsSlider.forEach(icon => new IconSlide(icon));
-
 
 
