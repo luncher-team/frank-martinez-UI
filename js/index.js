4caf42edcdfa610a7d73ea7213dc1c5fd950202c
Frank Martinez <frankie.martinez@gmail.com>
2019-02-06 10:00:17 -0800

    scroll position animation

--- js/index.js
+++ js/index.js
@@ -1,23 +1,84 @@
+let last_known_scroll_position = 0;
+let ticking = false;
+
 
 // Build a component because that's what we do
-class Something {
-    constructor(element) {
-        this.element = element;
-    }
+class IconSlide {
+  constructor(icon) {
+    this.icon = icon;
+    this.scrollPos = last_known_scroll_position;
+
+    this.iconLeft = document.querySelectorAll('.icon-left');
+    this.textLeft = document.querySelectorAll('.text-left');
+    
+    this.iconRight = document.querySelectorAll('.icon-right');
+    this.textRight = document.querySelectorAll('.text-right');
+    
+    this.iconLeft.forEach(icon => { 
+      if (this.scrollPos > 450) {
+        icon.style = 'left: 0';
+      }
+      if (this.scrollPos < 400) {
+        icon.style = 'left: -1000px';
+      }
+    });
+
+    this.textLeft.forEach(text => { 
+      if (this.scrollPos > 400) {
+        text.style = 'left: 0';
+      }
+      if (this.scrollPos < 375) {
+        text.style = 'left: -10000px';
+      }
+    });
+
+    this.iconRight.forEach(icon => { 
+      if (this.scrollPos > 550) {
+        icon.style = 'right: 0';
+      }
+      if (this.scrollPos < 525) {
+        icon.style = 'right: -1000px';
+      }
+    });
+
+    this.textRight.forEach(text => { 
+      if (this.scrollPos > 500) {
+        text.style = 'right: 0';
+      }
+      if (this.scrollPos < 475) {
+        text.style = 'right: -1000px';
+      }
+    });
+
+
+  }
 }
 
 
+// Scroll Position
+// Shove in the last known position
+function doSomething(scroll_pos) {
+  console.log(scroll_pos);
+  scroll_pos = new IconSlide(last_known_scroll_position);
+}
+
+window.addEventListener('scroll', function(e) {
+  last_known_scroll_position = window.scrollY;
+
+  if (!ticking) {
+    window.requestAnimationFrame(function() {
+      doSomething(last_known_scroll_position);
+      ticking = false;
+    });
+
+    ticking = true;
+  }
+});
 
 
 // Select some stuff
-const iconsLeft = document.querySelectorAll('.icon-left');
-iconsLeft.forEach(icon => console.log(icon));
+const iconsSlider = document.querySelectorAll('.icon-slider');
+iconsSlider.forEach(icon => new IconSlide(icon));
 
 
 
-
-
-
-
-
-// iconsLeft.forEach( link => new TabLink(link) );
